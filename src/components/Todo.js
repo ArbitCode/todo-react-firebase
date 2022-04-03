2494e41211763d7768649f700a47d5bd77c34eaf
Raja Ram Sharma <Rajaramsharma422@gmail.com>
2022-04-03 14:49:09 +0530

    basic todo react app

---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import {List, ListItem, ListItemAvatar, ListItemText} from '@material-ui/core';
+import { DeleteForever } from '@material-ui/icons/';
+import React from "react";
+import { db } from '../firebase';
+import './Todo.css'
+
+export const Todo = ({arr})=>{
+    return (
+        <List className="todo__list">
+            <ListItem>
+                <ListItemAvatar/>
+                <ListItemText primary={arr.item.todo} secondary={arr.item.todo}/>
+            </ListItem>
+            <DeleteForever fontSize='large' onClick = {()=> {db.collection('todos').doc(arr.id).delete()}}/>
+        </List>
+    )
+}
